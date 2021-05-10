c6ea19d7cfb1521a5e913103857cd77096b2d09d
최상인 <sangin3435@gmail.com>
2021-05-10 14:12:34 +0900

    Simple Job 생성

---
+++ src/main/java/com/locky/spring/springbatchinaction/job/SimpleJobConfiguration.java
@@ -0,0 +1,58 @@
+package com.locky.spring.springbatchinaction.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j // log 사용을 위한 lombok 어노테이션
+@RequiredArgsConstructor // 생성자 DI를 위한 lombok 어노테이션
+@Configuration //Batch의 모든 Job은 @Configuration으로 등록해서 사용한다.
+public class SimpleJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory; // 생성자 DI 받음
+    private final StepBuilderFactory stepBuilderFactory; // 생성자 DI 받음
+
+    @Bean
+    public Job simpleJob() {
+        // simpleJob 이란 이름의 Batch Job을 생성한다. job의 이름은 별도로 지정하지 않고, Builder를 통해 지정한다.
+        return jobBuilderFactory.get("simpleJob")
+                .start(simpleStep1(null))
+                .next(simpleStep2(null))
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    // Batch가 수행되면 log.info(">>>>> This is Step1") 가 출력된다.
+                    log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2")
+                // Step 안에서 수행될 기능들을 명시한다.
+                // Tasklet은 Step안에서 단일로 수행될 커스텀한 기능등릉 선언할때 사용한다.
+                .tasklet((contribution, chunkContext) -> {
+                    // Batch가 수행되면 log.info(">>>>> This is Step1") 가 출력된다.
+                    log.info(">>>>> This is Step2");
+                    log.info(">>>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
